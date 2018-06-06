31114f54767f307186892d40eff765192cc95a9d
Franklin Wu <franklin.w.wu@gmail.com>
2018-06-06 02:39:47 -0700

    Complete 04-052-module exercise

---
+++ exercises/04-052-module/exercises.js
@@ -0,0 +1,43 @@
+"use strict";
+// Exercise 1
+// expected: 20
+// const, arrow functions
+var double = function (value) {
+    return value * 2;
+};
+console.log(double(10));
+// Exercise 2
+// expected: Hello, Max
+// expected: Hello, Anna
+// const, default parameters, arrow functions
+var greet = function (name) {
+    if (name === undefined) {
+        name = "Max";
+    }
+    console.log("Hello, " + name);
+};
+greet();
+greet("Anna");
+// Exercise 3
+// expected: -3
+// const, spread operator
+var numbers = [-3, 33, 38, 5];
+console.log(Math.min.apply(Math, numbers));
+// Exercise 4
+// expected: [55, 20, -3, 33, 38, 5]
+// const, spread operator
+var newArray = [55, 20];
+newArray.push.apply(newArray, numbers);
+console.log(newArray);
+// Exercise 5
+// expected: 3.89 2.99 1.38
+// const, array destructuring
+var testResults = [3.89, 2.99, 1.38];
+var result1 = testResults[0], result2 = testResults[1], result3 = testResults[2];
+console.log(result1, result2, result3);
+// Exercise 6
+// expected: Will 12
+// const, object destructuring
+var scientist = { firstName: "Will", experience: 12 };
+var firstName = scientist.firstName, experience = scientist.experience;
+console.log(firstName, experience);
---
+++ exercises/04-052-module/exercises.ts
@@ -0,0 +1,45 @@
+// Exercise 1
+// expected: 20
+// const, arrow functions
+const double: (arg1: number) => number = function(value: number) {
+    return value * 2;
+};  
+console.log(double(10));
+
+// Exercise 2
+// expected: Hello, Max
+// expected: Hello, Anna
+// const, default parameters, arrow functions
+const greet: (arg1?: string) => void = function (name?: string) {
+    if (name === undefined) { name = "Max"; }
+    console.log("Hello, " + name);
+};
+greet();
+greet("Anna");
+
+// Exercise 3
+// expected: -3
+// const, spread operator
+const numbers = [-3, 33, 38, 5];
+console.log(Math.min(...numbers));
+
+// Exercise 4
+// expected: [55, 20, -3, 33, 38, 5]
+// const, spread operator
+const newArray = [55, 20];
+newArray.push(...numbers);
+console.log(newArray);
+
+// Exercise 5
+// expected: 3.89 2.99 1.38
+// const, array destructuring
+const testResults = [3.89, 2.99, 1.38];
+const [result1, result2, result3] = testResults;
+console.log(result1, result2, result3);
+
+// Exercise 6
+// expected: Will 12
+// const, object destructuring
+const scientist = {firstName: "Will", experience: 12};
+const {firstName, experience} = scientist;
+console.log(firstName, experience);
