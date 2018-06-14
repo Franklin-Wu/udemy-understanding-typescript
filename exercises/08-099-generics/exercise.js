4e557fa4954ebcb0b9ec0856a16ac0522bc81033
Franklin Wu <franklin.w.wu@gmail.com>
2018-06-14 04:00:04 -0700

    Add 08-099-generics exercise starter code

---
+++ exercises/08-099-generics/exercise.js
@@ -0,0 +1,6 @@
+"use strict";
+const map = new Map();
+map.set(0, 'hello generics');
+map.set(1, 'goodbye generics');
+console.log(map.get(0));
+console.log(map.get(1));
---
+++ exercises/08-099-generics/exercise.ts
@@ -0,0 +1,5 @@
+const map = new Map<number, string>();
+map.set(0, 'hello generics');
+map.set(1, 'goodbye generics');
+console.log(map.get(0));
+console.log(map.get(1));
