ede873bb60c9ed47fd3421514778ffce54ecd3cb
Franklin Wu <franklin.w.wu@gmail.com>
2018-06-15 01:22:22 -0700

    Complete 08-099-generics exercise

--- exercises/08-099-generics/exercise.js
+++ exercises/08-099-generics/exercise.js
@@ -1,6 +1,42 @@
 "use strict";
-const map = new Map();
-map.set(0, 'hello generics');
-map.set(1, 'goodbye generics');
-console.log(map.get(0));
-console.log(map.get(1));
+class MyMap {
+    constructor() {
+        this.dictionary = {};
+    }
+    // should create a new key-value pair
+    setItem(key, item) {
+        this.dictionary[key] = item;
+    }
+    // should retrieve the value of the provided key
+    getItem(key) {
+        return this.dictionary[key];
+    }
+    // should remove all key-value pairs
+    clear() {
+        this.dictionary = {};
+    }
+    // should output key-value pairs
+    printMap() {
+        for (let key in this.dictionary) {
+            console.log(key + ': ' + this.getItem(key));
+        }
+    }
+}
+const numberMap = new MyMap();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+console.log(numberMap.getItem('apples'));
+console.log(numberMap.getItem('bananas'));
+console.log(numberMap.getItem('cherries'));
+numberMap.printMap();
+numberMap.clear();
+numberMap.printMap();
+const stringMap = new MyMap();
+stringMap.setItem('name', 'Max');
+stringMap.setItem('age', '27');
+console.log(stringMap.getItem('age'));
+console.log(stringMap.getItem('name'));
+console.log(stringMap.getItem('occupation'));
+stringMap.printMap();
+stringMap.clear();
+stringMap.printMap();
--- exercises/08-099-generics/exercise.ts
+++ exercises/08-099-generics/exercise.ts
@@ -1,5 +1,45 @@
-const map = new Map<number, string>();
-map.set(0, 'hello generics');
-map.set(1, 'goodbye generics');
-console.log(map.get(0));
-console.log(map.get(1));
+class MyMap<T> {
+    private dictionary: {[key: string]: T} = {};
+
+    // should create a new key-value pair
+    setItem(key: string, item: T): void {
+        this.dictionary[key] = item;
+    }
+
+    // should retrieve the value of the provided key
+    getItem(key: string): T {
+        return this.dictionary[key];
+    }
+
+    // should remove all key-value pairs
+    clear(): void {
+        this.dictionary = {};
+    }
+
+    // should output key-value pairs
+    printMap(): void {
+        for (let key in this.dictionary) {
+            console.log(key + ': ' + this.getItem(key));
+        }
+    }
+}
+
+const numberMap = new MyMap<number>();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+console.log(numberMap.getItem('apples'))
+console.log(numberMap.getItem('bananas'))
+console.log(numberMap.getItem('cherries'))
+numberMap.printMap();
+numberMap.clear();
+numberMap.printMap();
+
+const stringMap = new MyMap<string>();
+stringMap.setItem('name', 'Max');
+stringMap.setItem('age', '27');
+console.log(stringMap.getItem('age'))
+console.log(stringMap.getItem('name'))
+console.log(stringMap.getItem('occupation'))
+stringMap.printMap();
+stringMap.clear();
+stringMap.printMap();
